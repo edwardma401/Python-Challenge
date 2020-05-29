0f65b219ab663854a5b8b54e441e88f1ce9c0d2c
edwardma401 <64027921+edwardma401@users.noreply.github.com>
2020-05-29 17:26:20 -0400

    Finished PyPoll in 50 minutesgit add -A YAY

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,28 +1,84 @@
-# ou will be give a set of poll data called election_data.csv. The dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+# The dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+#                                             row[0]   row[1]      row[3]
 
+import os
+os.chdir('C:\\Users\\mahoy\\Documents\\GitHub\\Python-Challenge\\PyPoll')
 
 # The total number of votes cast
 
+from csv import reader 
+with open("Resources/electron_data.csv") as read_obj: 
+    csv_reader = reader(read_obj) 
+    csv_reader = list(csv_reader)
+    totalvotes = len(csv_reader[1:])
 
 # A complete list of candidates who received votes
+vforkhan = 0
+vforcorrey = 0
+vforli = 0
+vfortooly = 0
+candidates = []
 
+from csv import reader 
+with open("Resources/electron_data.csv") as read_obj:
+    csv_reader = reader(read_obj)
+    next(csv_reader)
+    for row in csv_reader:
+        if str(row[2]) == "Khan":
+            vforkhan += 1
+        elif str(row[2]) == "Correy":
+            vforcorrey += 1
+        elif str(row[2]) == "Li":
+            vforli += 1
+        else:
+            vfortooly += 1
+    candidates.append(vforkhan)
+    candidates.append(vforcorrey)
+    candidates.append(vforli)
+    candidates.append(vfortooly)
+    winner = max(candidates)
+    if vforcorrey == winner:
+        thewinner = "Khan"
+    elif vforli == winner:
+        thewinner = "Li"
+    elif vfortooly == winner:
+        thewinner = "O'Tooley"
+    elif vforkhan == winner:
+        thewinner = "Khan"
+kpercent = round(vforkhan / totalvotes * 100, 3)
+cpercent = round(vforcorrey / totalvotes * 100, 3)
+lpercent = round(vforli / totalvotes * 100, 3)
+opercent = round(vfortooly / totalvotes * 100, 3)
 
-# The percentage of votes each candidate won
+print(f"""
+Election Results
+-------------------------
+Total Votes: {totalvotes}
+-------------------------
+Khan: {kpercent}% ({vforkhan})
+Correy: {cpercent}% ({vforcorrey})
+Li: {lpercent}% ({vforli})
+O'Tooley: {opercent}% ({vfortooly})
+-------------------------
+Winner: {thewinner}
+-------------------------
+""")
 
+os.chdir('C:\\Users\\mahoy\\Documents\\GitHub\\Python-Challenge\\PyPoll\\analysis')
+Result_txt = open("Result.txt", "w", encoding='utf-8')
 
-# The total number of votes each candidate won
+print(f"""
+Election Results
+-------------------------
+Total Votes: {totalvotes}
+-------------------------
+Khan: {kpercent}% ({vforkhan})
+Correy: {cpercent}% ({vforcorrey})
+Li: {lpercent}% ({vforli})
+O'Tooley: {opercent}% ({vfortooly})
+-------------------------
+Winner: {thewinner}
+-------------------------
+""", file=Result_txt)
 
-
-# The winner of the election based on popular vote.
-
-# Election Results
-# -------------------------
-# Total Votes: 3521001
-# -------------------------
-# Khan: 63.000% (2218231)
-# Correy: 20.000% (704200)
-# Li: 14.000% (492940)
-# O'Tooley: 3.000% (105630)
-# -------------------------
-# Winner: Khan
-# -------------------------
+Result_txt.close()
